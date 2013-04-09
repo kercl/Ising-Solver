907ffc8e3e3ec4f3252788ec4730dcdca9ce8330
koe- <clemens.kerschbaumer@gmail.com>
2013-04-09 20:52:30 +0200

    readme

--- README.mk
+++ README.mk
@@ -1,1 +1,5 @@
+ISING SOLVER
+============
 
+This program is designed to numerically find minimal energy configurations
+in the Ising model using genetic algorithms.
--- graph/graph.h
+++ graph/graph.h
@@ -1,4 +1,7 @@
 #ifndef GRAPH_H
 #define GRAPH_H
 
+struct  {
+	
+
 #endif
